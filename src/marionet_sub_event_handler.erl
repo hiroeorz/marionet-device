fff40a029efedc87a393227252146deded41b742
HIROE Shin <hiroe.orz@gmail.com>
2014-01-24 09:25:50 +0000

    add subscribe event

--- src/marionet_sub_event_handler.erl
+++ src/marionet_sub_event_handler.erl
@@ -55,14 +55,14 @@ handle_event({connack_accept}, State=#state{subs=Subscribes}) ->
 handle_event({publish, <<"marionet/1/digital/0">> = Topic, Payload}, State) ->
     lager:info("publish: topic:~p~n", [Topic]),
     [16#01, _PortNo, StateList] = marionet_data:unpack(Payload),
-    [_, _, _, _, _, _, State, _] = StateList,
+    [_, State, _, _, _, _, _, _] = StateList,
     gpio_pin:write(25, State),
     {ok, State};
 
 handle_event({publish, <<"marionet/2/digital/0">> = Topic, Payload}, State) ->
     lager:info("publish: topic:~p~n", [Topic]),
     [16#01, _PortNo, StateList] = marionet_data:unpack(Payload),
-    [_, _, _, _, _, _, State, _] = StateList,
+    [_, State, _, _, _, _, _, _] = StateList,
     gpio_pin:write(25, State),
     {ok, State};
 
