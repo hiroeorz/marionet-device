2f2c99bffb6ead84fa1e671ef0b78c34da3091d8
Shin Hiroe <hiroe.orz@gmail.com>
2015-07-08 15:53:16 +0900

    状態取得API

--- src/marionet_device.erl
+++ src/marionet_device.erl
@@ -9,7 +9,11 @@
 -module(marionet_device).
 
 %% API
--export([start/0]).
+-export([start/0,
+	 all_digital/0,
+	 digital_multi_value/1,
+	 all_analog/0,
+	 analog_multi_value/1]).
 
 %%%===================================================================
 %%% API
@@ -28,3 +32,35 @@ start() ->
     application:start(cowboy),
     application:start(erlzmq),
     application:start(marionet_device).
+
+%%--------------------------------------------------------------------
+%% @doc get all digital state.
+%% @end
+%%--------------------------------------------------------------------
+-spec all_digital() -> [0 | 1].
+all_digital() ->
+    marionet_device_status:all_digital().
+
+%%--------------------------------------------------------------------
+%% @doc get digital value list, from digital no list.
+%% @end
+%%--------------------------------------------------------------------
+-spec digital_multi_value([non_neg_integer()]) -> [0 | 1].
+digital_multi_value(DigitalNoList) ->
+    marionet_device_status:digital_multi_value(DigitalNoList).
+
+%%--------------------------------------------------------------------
+%% @doc get all digital state.
+%% @end
+%%--------------------------------------------------------------------
+-spec all_analog() -> [non_neg_integer()].
+all_analog() ->
+    marionet_device_status:all_analog().
+
+%%--------------------------------------------------------------------
+%% @doc get analog value list, from analog no list.
+%% @end
+%%--------------------------------------------------------------------
+-spec analog_multi_value([non_neg_integer()]) -> [non_neg_integer()].
+analog_multi_value(AnalogNoList) ->
+    marionet_device_status:analog_multi_value(AnalogNoList).
